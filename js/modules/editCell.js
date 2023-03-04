43b6747d6375be9f942c4c713a1d32da9b7813ca
Dmytro <kotykhin_d@ukr.net>
2023-03-04 18:45:54 +0100

    editCell update

--- js/modules/editCell.js
+++ js/modules/editCell.js
@@ -5,61 +5,63 @@ const tableBody = document.querySelector('.table__body').getElementsByTagName("t
 
 const editCell = (n) => {
     for (let i = 0; i < tableBody.length; i++) {
-        const td = tableBody[i].cells[n].querySelector('.editIcon');
-        if (td) {
-            const tdCell = tableBody[i];
-            const updatedValue = td.closest('tr').cells[n].innerText.trim();
-            const updatedProductName = tableBody[i].closest('tr').cells[0].innerText.trim();
-            const updatedPriceWithTax = tableBody[i].closest('tr').cells[4];
-            td.addEventListener('click', () => {
-                let div = document.createElement("div");
-                td.closest('tr').cells[n].innerHTML = ''
-                div.innerHTML = `
-                        <input class='updateInput' type="text" value='${updatedValue}' id='updateInput' />
-                        <div>
-                            <button class='tableButton cancelButton'>Cancel</button>
-                            <button class='tableButton updateButton'>Update</button>
-                        </div>
-                    `;
-                tdCell.cells[n].appendChild(div);
-                const updateButton = document.querySelector('.updateButton');
-                const cancelButton = document.querySelector('.cancelButton');
-                const updateInput = document.querySelector('.updateInput');
-                updateButton.addEventListener('click', () => {
-                    tdCell.cells[n].removeChild(div);
-                    tdCell.cells[n].innerHTML = `
-                                ${updateInput.value}
+        const cell = tableBody[i].cells[n];   
+        if (cell) {
+            const editIcon = cell.querySelector('.editIcon');        
+            if (editIcon) {
+                const updatedValue = cell.innerText.trim();                
+                const updatedProductName = tableBody[i].cells[0].innerText.trim();
+                const updatedPriceWithTax = tableBody[i].cells[4];
+                editIcon.addEventListener('click', () => {
+                    let div = document.createElement("div");
+                    cell.innerHTML = '';
+                    div.innerHTML = `
+                            <input class='updateInput' type="text" value='${updatedValue}' id='updateInput' />
+                            <div>
+                                <button class='tableButton cancelButton'>Cancel</button>
+                                <button class='tableButton updateButton'>Update</button>
+                            </div>
+                        `;
+                    cell.appendChild(div);
+                    const updateButton = document.querySelector('.updateButton');
+                    const cancelButton = document.querySelector('.cancelButton');
+                    const updateInput = document.querySelector('.updateInput');
+                    updateButton.addEventListener('click', () => {
+                        cell.removeChild(div);
+                        cell.innerHTML = `
+                                    ${updateInput.value}
+                                    ${constants.editIcon}
+                                `;
+                        const localStorageData = localStorage.getItem('tableData');
+                        const dataArray = JSON.parse(localStorageData);
+                        if (dataArray) {
+                            const modifiedItem = dataArray.find(item => item.name === updatedProductName);
+                            const restArray = dataArray.filter(item => item.name !== updatedProductName);
+                            if (n === 0) {
+                                modifiedItem.name = updateInput.value;
+                            } else if (n === 2) {
+                                modifiedItem.quantity = updateInput.value;
+                                updatedPriceWithTax.innerHTML = modifiedItem.quantity * modifiedItem.price * 1.25
+                                sumValue();
+                            } else if (n === 3) {
+                                modifiedItem.price = updateInput.value;
+                                updatedPriceWithTax.innerHTML = modifiedItem.quantity * modifiedItem.price * 1.25
+                                sumValue();
+                            }
+                            const newArray = [modifiedItem, ...restArray];
+                            localStorage.setItem('tableData', JSON.stringify(newArray));
+                            editCell(n);
+                        }
+                    })
+                    cancelButton.addEventListener('click', () => {
+                        cell.innerHTML = `
+                                ${updatedValue}
                                 ${constants.editIcon}
                             `;
-                    const localStorageData = localStorage.getItem('tableData');
-                    const dataArray = JSON.parse(localStorageData);
-                    if (dataArray) {
-                        const modifiedItem = dataArray.find(item => item.name === updatedProductName);
-                        const restArray = dataArray.filter(item => item.name !== updatedProductName);
-                        if (n === 0) {
-                            modifiedItem.name = updateInput.value;
-                        } else if (n === 2) {
-                            modifiedItem.quantity = updateInput.value;
-                            updatedPriceWithTax.innerHTML = modifiedItem.quantity * modifiedItem.price * 1.25
-                            sumValue();
-                        } else if (n === 3) {
-                            modifiedItem.price = updateInput.value;
-                            updatedPriceWithTax.innerHTML = modifiedItem.quantity * modifiedItem.price * 1.25
-                            sumValue();
-                        }
-                        const newArray = [modifiedItem, ...restArray];
-                        localStorage.setItem('tableData', JSON.stringify(newArray));
                         editCell(n);
-                    }
+                    })
                 })
-                cancelButton.addEventListener('click', () => {
-                    tdCell.cells[n].innerHTML = `
-                            ${updatedValue}
-                            ${constants.editIcon}
-                        `;
-                    editCell(n);
-                })
-            })
+            }
         }
     }
 };
